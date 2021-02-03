8d31802e4a2685a85134ac53799df0ce057a3d2c
Valentin Lukovski <47025947+VLukovski@users.noreply.github.com>
2021-02-03 13:05:03 +0000

    SAR-7051 Added timeout and exit survey, beta feedback and report problem URLs

---
+++ app/uk/gov/hmrc/claimvatenrolmentfrontend/controllers/SignInOutController.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.claimvatenrolmentfrontend.controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions}
+import uk.gov.hmrc.claimvatenrolmentfrontend.config.AppConfig
+
+import scala.concurrent.Future
+
+@Singleton
+class SignInOutController @Inject()(val controllerComponents: ControllerComponents,
+                                    val authConnector: AuthConnector,
+                                    appConfig: AppConfig)
+  extends BaseController with AuthorisedFunctions{
+
+  def postSignIn: Action[AnyContent] = Action.async {
+    _ => Future.successful(Redirect(routes.IndexController.show().url))
+  }
+
+  def signOut: Action[AnyContent] = Action.async {
+    _ => Future.successful(Redirect(appConfig.feedbackUrl).withNewSession)
+  }
+
+  def keepAlive: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok.withSession(request.session))
+  }
+
+}
