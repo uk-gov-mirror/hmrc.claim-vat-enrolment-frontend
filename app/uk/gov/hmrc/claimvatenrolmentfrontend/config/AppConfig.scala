a39c74bc97351f758d085727b750f2151aff3768
msatta <3033136+msatta@users.noreply.github.com>
2026-09-09 13:55:47 +0100

    [DL-17675] fix UR banner feature flag

--- app/uk/gov/hmrc/claimvatenrolmentfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/claimvatenrolmentfrontend/config/AppConfig.scala
@@ -88,6 +88,6 @@ class AppConfig @Inject()(config: Configuration, servicesConfig: ServicesConfig)
 
   val knownFactsLockAttemptLimit: Int = 3
 
-  lazy val urBannerEnabled: Boolean = servicesConfig.getBoolean("feature-switch.knownFactsCheckWithVanFlag")
+  lazy val urBannerEnabled: Boolean = servicesConfig.getBoolean("feature-switch.urBannerFlag")
   lazy val urBannerBaseUrl: String = loadConfig("urBannerBaseUrl")
 }
